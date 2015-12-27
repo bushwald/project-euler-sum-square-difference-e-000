90720461f23f9d224f4d6bfcf61259d4503a3993
bushwald <bradley.allen84@gmail.com>
2015-12-27 12:22:27 -0500

    solutions implemented

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,35 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+	def initialize(num)
+		@num = num
+		@sumSquares = Array.new
+		@squareSum = Array.new
+		@sumSquaresSum = 0
+		@squareSumSum = 0
+	end
+
+	def difference
+		j = 1
+		for i in 0..@num-1
+			@sumSquares << j * j
+			@squareSum << j
+			j += 1
+		end
+
+		@sumSquares.each{ |x|
+			@sumSquaresSum += x
+		}
+
+		@squareSum.each{ |y|
+			@squareSumSum += y
+		}
+
+		@squareSumSum *= @squareSumSum
+
+		return @squareSumSum - @sumSquaresSum
+	end
+
+end
+
+puts SumSquareDifference.new(5).difference
+
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,27 @@
 # Implement your procedural solution here!
+def sum_square_difference(num)
+	sumSquares = Array.new
+	squareSum = Array.new
+	sumSquaresSum = 0
+	squareSumSum = 0
+
+	j = 1
+	for i in 0..num-1
+		sumSquares << j * j
+		squareSum << j
+		j += 1
+	end
+
+	sumSquares.each{ |x|
+		sumSquaresSum += x
+	}
+
+	squareSum.each{ |y|
+		squareSumSum += y
+	}
+
+	squareSumSum *= squareSumSum
+
+	return squareSumSum - sumSquaresSum
+
+end
